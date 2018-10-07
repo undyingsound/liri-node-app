945e89fcb2efbb4178277cd8d8019496d0c5cbaa
Mike Reza <mikerza@gmail.com>
2018-10-07 15:48:38 -0500

    spotify

--- liri.js
+++ liri.js
@@ -1,3 +1,18 @@
-let dotPack = require("dotenv").config();
+require("dotenv").config();
+
 let keys = require("keys").config();
 let spotify = new Spotify(keys.spotify);
+
+
+
+let spotify = require("./keys");
+let spotifyRequest = process.argv[2];
+
+spotify.search({ type: 'track', query: spotifyRequest }, function(err, data) {
+  if ( err ) {
+      console.log('Error occurred: ' + err);
+      return;
+  }
+  console.log(data);
+  
+});

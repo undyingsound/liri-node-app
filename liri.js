3680f741f083267229d0d8fc5bfe6df4c69d0ba3
Mike Reza <mikerza@gmail.com>
2018-10-12 20:08:24 -0500

    do what it says added

--- liri.js
+++ liri.js
@@ -3,10 +3,12 @@ require("dotenv").config();
 //apps
 let request = require("request");
 let Spotify = require("node-spotify-api");
+let moment = require('moment');
+let fs = require("fs");
 
 //keys
-var keys = require("./keys.js");
-var spotify = new Spotify(keys.spotify);
+let keys = require("./keys.js");
+let spotify = new Spotify(keys.spotify);
 
 //grabs arguments
 let entry = process.argv;
@@ -32,6 +34,10 @@ switch (input) {
 
     case "do-what-it-says":
         DWIS();
+        break;
+
+    case "concert-this":
+        concert();
         break;
 
     default:
@@ -97,8 +103,8 @@ function movieResult() {
 
     request(queryURL, function (error, response, body) {
         if (!error && response.statusCode === 200 && body) {
-                let data = JSON.parse(body);
-                let movieData =
+            let data = JSON.parse(body);
+            let movieData =
                 "\nTitle : " + data.Title +
                 "\nRelease Year : " + data.Year +
                 "\nIMDB Rating : " + data.imdbRating +
@@ -107,10 +113,10 @@ function movieResult() {
                 "\nLanguage : " + data.Language +
                 "\nPlot : " + data.Plot +
                 "\nFeatured Actors : " + data.Actors
-                console.log(movieData);
-                if (data.Error == 'Movie not found!') {
-                    console.log("Try another title, please.");
-                };
+            console.log(movieData);
+            if (data.Error == 'Movie not found!') {
+                console.log("Try another title, please.");
+            };
         } else if (data.Ratings.length < 2) {
             let movieData =
                 "\nTitle : " + data.Title +
@@ -121,13 +127,13 @@ function movieResult() {
                 "\nLanguage : " + data.Language +
                 "\nPlot : " + data.Plot +
                 "\nFeatured Actors : " + data.Actors
-           
+
             if (err) {
                 console.log("No data available");
             };
             console.log(movieData);
         }
-    else if (data.Ratings[1].Value !== undefined) {
+        else if (data.Ratings[1].Value !== undefined) {
             let movieData =
                 "\nTitle : " + data.Title +
                 "\nRelease Year : " + data.Year +
@@ -137,7 +143,7 @@ function movieResult() {
                 "\nLanguage : " + data.Language +
                 "\nPlot : " + data.Plot +
                 "\nFeatured Actors : " + data.Actors
-            
+
             if (err) {
                 console.log("No data available");
             };
@@ -146,3 +152,67 @@ function movieResult() {
     });
 
 };
+
+//BandsInTown
+
+//If no band is entered, ask in the right format
+function concert() {
+    if (process.argv[3] === undefined) {
+        console.log("Please enter an artist/band name.");
+        concertResult();
+        //Adjust for BIT Search
+    } else if (process.argv[3] !== undefined) {
+        //    titleAdjust = title.split(" ");
+        //    title = titleAdjust.join("+");
+        concertResult();
+    };
+};
+
+function concertResult() {
+    let arg = process.argv[3];
+    let titleAdjust = arg.split("-");
+    let title = titleAdjust.join("+");
+    let date = moment(date).format("MM/DD/YYYY");
+    var queryURL = "https://rest.bandsintown.com/artists/" + title + "/events?app_id=codingbootcamp";
+
+    request(queryURL, function (error, response, body) {
+
+        let data = JSON.parse(body);
+
+        for (var i = 0; i < data.length; i++) {
+            if (!error && response.statusCode === 200) {
+
+                console.log("Artist Name: " + data[i].artist.name);
+                console.log("Venue Name: " + data[i].venue.name);
+                console.log("Date: " + date);
+                console.log("Location: " + data[i].venue.city + ", " + data[i].venue.country);
+
+            }
+            else if (error) {
+                console.log(error);
+            };
+
+        }
+
+    });
+}
+
+
+//Do What It Says
+
+function DWIS() {
+    fs.readFile("random.txt", "utf8", function (err, data) {
+        if (err) {
+            console.log("There was an error. Try Again.");
+        }
+            let output = data.split(",");
+            let action = output[0];
+            process.argv[3] = output[1];
+            title = process.argv[3];
+    if (action === 'spotify-this-song') {
+        STSsearch();
+    };
+
+    });
+}
+   
